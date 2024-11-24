b41c19b44dc43bc1050a68ff7a5b430c39fab520
ariaandika <andikaakun17@gmail.com>
2024-11-24 19:09:38 +0700

    add vault example

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+let three_quarter = window.innerHeight - (window.innerHeight / 4);
+let half_screen = window.innerHeight / 2;
+let a_quarter = (window.innerHeight / 4);
 
 document.addEventListener("click", e => {
   let target = /** @type {HTMLButtonElement} */(e.target);
@@ -10,9 +12,44 @@ document.addEventListener("click", e => {
     },2000)
   }
 
+  if (target.hasAttribute("vault")) {
+    let vault = document.querySelector("#" + target.getAttribute("vault"));
+    vault.classList.toggle("vault-open");
+    vault.scrollTo({ top: three_quarter, behavior: "instant" })
+  }
 
 })
 
+document.addEventListener("keydown", e => {
+  let key = e.key;
 
+  if (key == "Escape") {
+    closeAllVault();
+  }
+
+})
+
+function closeAllVault() {
+  document.querySelectorAll(".vault-open").forEach(e => {
+    e.classList.toggle("vault-open")
+  })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  let vault = document.querySelector(".vault");
+  onVault(vault);
+})
+
+function onVault(vault) {
+  vault.addEventListener("scrollend", () => {
+    let scroll = vault.scrollTop;
+
+    if (scroll < a_quarter) {
+      closeAllVault()
+    } else if (scroll < three_quarter) {
+      vault.scrollTo({ top: three_quarter })
+    }
+  })
+}
 
 
